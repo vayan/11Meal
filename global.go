739c66e34aacd4c1b724f5e80b962669d9f8c889
Yann Vaillant <yann@vaillant.im>
2014-04-29 09:25:37 +0800

    add some error handling

--- global.go
+++ global.go
@@ -16,32 +16,28 @@ type Global interface {
 }
 
 func get(r Global) Global {
-	log.Println("before selected ", r, "is type ", reflect.TypeOf(r))
+	log.Println("get ", r.getID())
 	obj, err := dbmap.Get(reflect.ValueOf(r).Interface(), r.getID())
 	checkErr(err, "get resto failed")
-	log.Println("after selected ", obj)
+	if obj == nil {
+		return nil
+	}
 	return obj.(Global)
 }
 
 func add(r Global) {
-	log.Println("add new resto")
 	err := dbmap.Insert(r)
 	checkErr(err, "Insert resto failed")
-	log.Println("new resto added", r)
 }
 
 func del(r Global) {
-	log.Println("del resto", r)
-	count, err := dbmap.Delete(r)
+	_, err := dbmap.Delete(r)
 	checkErr(err, "Del resto failed")
-	log.Println("resto deleted:", count)
 }
 
-func put(r Global) { //update
-	log.Println("update resto", r)
-	count, err := dbmap.Update(r)
+func put(r Global) {
+	_, err := dbmap.Update(r)
 	checkErr(err, "Update resto failed")
-	log.Println("resto updated:", count)
 }
 
 func getObj(t string) Global {
@@ -66,40 +62,43 @@ func handleglobal(res http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	var obj = getObj(vars["table"])
 	var data []byte
-
-	switch req.Method {
-	case "POST":
-		err := req.ParseForm()
-		checkErr(err, "Error parse form")
-		err = decoder.Decode(obj, req.PostForm)
-		checkErr(err, "Error decode form")
-		add(obj)
-	case "GET":
-		d, err := strconv.Atoi(vars["id"])
-		if err != nil {
-			test := obj.getAll()
-			log.Println("result ", test)
-		} else {
-			log.Println("seting id to ", d)
+	data, _ = json.Marshal("{success:false}")
+	if obj != nil {
+		switch req.Method {
+		case "POST":
+			err := req.ParseForm()
+			checkErr(err, "Error parse form")
+			err = decoder.Decode(obj, req.PostForm)
+			checkErr(err, "Error decode form")
+			add(obj)
+			data, _ = json.Marshal(obj)
+		case "GET":
+			d, err := strconv.Atoi(vars["id"])
+			if err != nil {
+				test := obj.getAll()
+				data, _ = json.Marshal(test)
+			} else {
+				obj = obj.setID(d)
+				obj = get(obj)
+				if obj != nil {
+					data, _ = json.Marshal(obj)
+				}
+			}
+		case "DELETE":
+			d := atoi(vars["id"])
 			obj = obj.setID(d)
-			log.Println("id is now ", obj.getID())
-			obj = get(obj)
-
+			del(obj)
+			data, _ = json.Marshal("{success:true}")
+		case "PUT":
+			d := atoi(vars["id"])
+			err := req.ParseForm()
+			checkErr(err, "Error parse form")
+			err = decoder.Decode(obj, req.PostForm)
+			checkErr(err, "Error decode form")
+			obj = obj.setID(d)
+			put(obj)
+			data, _ = json.Marshal(obj)
 		}
-	case "DELETE":
-		d := atoi(vars["id"])
-		obj = obj.setID(d)
-		del(obj)
-
-	case "PUT":
-		d := atoi(vars["id"])
-		err := req.ParseForm()
-		checkErr(err, "Error parse form")
-		err = decoder.Decode(obj, req.PostForm)
-		checkErr(err, "Error decode form")
-		obj = obj.setID(d)
-		put(obj)
 	}
-	data, _ = json.Marshal(obj)
 	res.Write(data)
 }

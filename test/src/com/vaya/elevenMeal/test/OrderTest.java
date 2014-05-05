f2fc7784be0800014aff6c2355e819d7c7fcbdd2
Emmanuel <ohland_e@epitech.net>
2014-05-05 23:53:38 +0800

    Add test project with Order unit test

---
+++ test/src/com/vaya/elevenMeal/test/OrderTest.java
@@ -0,0 +1,54 @@
+package com.vaya.elevenMeal.test;
+
+import java.util.ArrayList;
+
+import android.util.Log;
+
+import com.vaya.elevenMeal.Meal;
+import com.vaya.elevenMeal.Order;
+
+import junit.framework.TestCase;
+
+public class OrderTest extends TestCase {
+	protected Meal mMeal1;
+	protected Meal mMeal2;
+	protected Order mOrder;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		mMeal1 = new Meal();
+		mMeal2 = new Meal();
+		mOrder = new Order();
+		
+		mMeal1.setName("Tomato Salad");
+		mMeal1.setPrice(10);
+		mMeal2.setName("Beef Steak");
+		mMeal2.setPrice(42);
+	}
+
+	public void testGetTotalPrice() {
+		mOrder.addMeal(mMeal1);
+		mOrder.addMeal(mMeal2);
+		float price = mMeal1.getPrice() + mMeal2.getPrice();
+		assertTrue(mOrder.getTotalPrice() == price);
+	}
+
+	public void testAddMeal() {
+		mOrder.addMeal(mMeal1);
+		mOrder.addMeal(mMeal2);
+		ArrayList<Meal> meals = (ArrayList<Meal>) mOrder.getMealList();
+		assertTrue(
+				meals.get(0).getName().equals(mMeal1.getName())
+			 && meals.get(1).getName().equals(mMeal2.getName()));
+	}
+
+	public void testRemoveMeal() {
+		mOrder.addMeal(mMeal1);
+		mOrder.addMeal(mMeal2);
+		Log.d("removeMeal", "coucou");
+		mOrder.removeMeal(mMeal2);
+		ArrayList<Meal> meals = (ArrayList<Meal>) mOrder.getMealList();
+		Log.d("removeMeal", "" + meals.size());
+		fail("to be continued...");
+	}
+}

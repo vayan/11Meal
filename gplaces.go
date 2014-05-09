eef724e3fb1828fed8d82d8aff1ed9c51009d792
Yann Vaillant <yann@vaillant.im>
2014-05-09 16:11:02 +0800

    cache completed, start handling multile page result

--- gplaces.go
+++ gplaces.go
@@ -11,24 +11,39 @@ import (
 
 // https://developers.google.com/places/documentation/search#PlaceSearchRequests
 
+func is_cached(hash string) bool {
+	i64, err := dbmap.SelectInt("select count(*) from restaurant where id_request=?", hash)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	if i64 > 0 {
+		log.Println("data already in DB")
+		return true
+	}
+	return false
+}
+
 func get_data() {
 	log.Println("=== Start getting data...")
 	location := "39.9075000,116.3972300"
 	h := GetMD5Hash(location)
-	log.Println("hash for request is", h)
-	restos, err := get_place_nearby(location, h)
-	if err != nil {
-		log.Panicln(err)
-	}
-	for _, resto := range restos {
-		descr := strings.Join(resto.Types, ", ")
-		log.Println(resto.Formatted_address)
-		r := Restaurant{
-			Name:        resto.Name,
-			Address:     resto.Formatted_address,
-			Id_request:  h,
-			Description: descr}
-		add(&r)
+	if !is_cached(h) {
+		log.Println("Get data from google")
+		restos, err := get_place_nearby(location, h)
+		if err != nil {
+			log.Panicln(err)
+		}
+		for _, resto := range restos {
+			descr := strings.Join(resto.Types, ", ")
+			log.Println(resto.Formatted_address)
+			r := Restaurant{
+				Name:        resto.Name,
+				Address:     resto.Formatted_address,
+				Id_request:  h,
+				Description: descr}
+			add(&r)
+		}
 	}
 	log.Println("=== Data restored")
 }
@@ -58,6 +73,9 @@ func get_place_nearby(location string, h string) ([]RestaurantGoogle, error) {
 		return restos, err
 	}
 	m := res.(map[string]interface{})
+	if m["next_page_token"] != nil {
+		log.Println("there is more pages")
+	}
 	if m["status"].(string) == "OK" {
 		data, err = json.Marshal(m["results"])
 		if err != nil {

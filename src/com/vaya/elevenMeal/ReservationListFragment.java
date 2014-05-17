b62536753f9577073c125aa4a9ce814eaae16f51
Emmanuel <ohland_e@epitech.net>
2014-05-17 22:30:27 +0800

    Add Reservation list (& change app name)

--- src/com/vaya/elevenMeal/ReservationListFragment.java
+++ src/com/vaya/elevenMeal/ReservationListFragment.java
@@ -1,15 +1,24 @@
 package com.vaya.elevenMeal;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.client.methods.HttpRequestBase;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.View;
+import android.webkit.WebView.FindListener;
 import android.widget.ListView;
 
+import com.vaya.elevenMeal.restaurant.IRestaurantObject;
 import com.vaya.elevenMeal.restaurant.Reservation;
+import com.vaya.elevenMeal.restaurant.Restaurant;
+import com.vaya.elevenMeal.restaurant.User;
 
 /**
  * A list fragment representing a list of Reservations. This fragment also
@@ -24,6 +33,7 @@ public class ReservationListFragment extends ListFragment
 	implements OnTaskCompleted {
 
 	private List<Reservation> reservationList;
+	private ListView	mListReservationView;
 
 	/**
 	 * The serialization (saved instance state) Bundle key representing the
@@ -69,18 +79,15 @@ public class ReservationListFragment extends ListFragment
 	 * fragment (e.g. upon screen orientation changes).
 	 */
 	public ReservationListFragment() {
-		//FIXME: Get id from preferences
-		//int id= this
-		//		.getActivity()
-		//		.getSharedPreferences("User", 0)
-		//		.getInt("id", 0);
-		int id = 5;
-		new API(this).get(new Reservation(), "user", String.valueOf(id));
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		SharedPreferences settings = getActivity().getSharedPreferences("11Meal",
+				Context.MODE_PRIVATE);
+		int id = settings.getInt("user_id", 0);
+		new API(this).get(new Reservation(), "user", String.valueOf(id));
 	}
 
 	@Override
@@ -156,14 +163,68 @@ public class ReservationListFragment extends ListFragment
 
 		mActivatedPosition = position;
 	}
+	
+	public class ResView {
+		public String mRestaurantName;
+		public String mOwnerName;
+
+		public ResView(String restaurantName, String ownerName) {
+			mRestaurantName = restaurantName;
+			mOwnerName = ownerName;
+		}
+	}
+
+	private class BuildResViewList extends AsyncTask<List<Reservation>, List<ResView>, List<ResView>> {
+
+		@SuppressWarnings("unchecked")
+		@Override
+		protected List<ResView> doInBackground(List<Reservation>... list) {
+			List<ResView> resViewList = new ArrayList<ResView>();
+			API api = new API();
+			api.setAsync(false);
+			for (Reservation r: reservationList) {
+				List<IRestaurantObject> resultList;
+				User owner = new User();
+				Restaurant restaurant = new Restaurant();
+
+				owner.setLogin(getString(R.string.unk_user));
+				restaurant.setName(getString(R.string.unk_restaurant));
+
+				api.get(new User(), "id", String.valueOf(r.getOwnerId()));
+				resultList = 
+						(List<IRestaurantObject> ) api.getLastResult();
+				if (resultList != null)
+					owner = User.class.cast(resultList.get(0));
+
+				api.get(new Restaurant(), "id", String.valueOf(r.getRestaurantId()));
+				resultList = 
+						(List<IRestaurantObject> ) api.getLastResult();
+				if (resultList != null)
+					restaurant = Restaurant.class.cast(resultList.get(0));
+
+				resViewList.add(new ResView(restaurant.getName(), owner.getLogin()));
+			}
+
+			return resViewList;
+		}
+
+		@Override
+		protected void onPostExecute(List<ResView> result) {
+			super.onPostExecute(result);
+			setListAdapter(new ReservationListAdapter(
+					getActivity(), R.layout.adapter_reservation_list, result));
+		}
+		
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void onTaskCompleted(Object res, java.lang.reflect.Type type) {
-		if (res == null)
+		if (res == null) {
+			//TODO: Show error to user
 			return ;
+		}
 		reservationList = (List<Reservation>) res;
-		setListAdapter(new ReservationListAdapter(
-				getActivity(), R.layout.adapter_reservation_list, reservationList));
+		new BuildResViewList().execute(reservationList);
 	}
 }
